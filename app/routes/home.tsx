e7c4b5ef31862379336edd6e8bd8cd1ec6ed07d8
sakab257 <slmbouskine93@gmail.com>
2025-09-06 20:28:27 +0200

    premier commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/resume-card";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Feedback intelligent pour votre future carrière!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Suivez vos candidatures & notez vos CVs</h1>
+        <h2>Examinez vos CVs et recevez un feedback fait par une IA !</h2>
+      </div>
+    </section>
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+    
+    
+  </main>;
 }
