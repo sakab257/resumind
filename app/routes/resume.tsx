a1be1a36d2fae689265c6ba8407174ccd35c9acf
sakab257 <slmbouskine93@gmail.com>
2025-09-11 14:47:12 +0200

    quelques console log qui trainent

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -45,7 +45,6 @@ const Resume = () => {
         setImageUrl(imageUrl);
 
         setFeedback(data.feedback);
-        console.log({resumeUrl, imageUrl, feedback: data.feedback});
       }
       loadResume();
     },[id]);
